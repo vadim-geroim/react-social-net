c1237998cdd2221a53a9926cc9b094f16f1b15c8
vgeroim <vgeroim.qa@gmail.com>
2021-11-20 19:01:38 -0600

    Extract JSX to file components

---
+++ src/header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="navbar">
+            <a href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+        </div>
+    );
+}
+
+export default Header;
