36bd927bd1feacedee78865bb490a388fff29378
vgeroim <vgeroim.qa@gmail.com>
2021-12-04 10:11:05 -0600

    Create subcomponents for Dialogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,28 +2,34 @@ import style from './Dialogs.module.css';
 import { Messages } from '../Messages/Messages';
 import { NavLink } from 'react-router-dom';
 
+const Dialog = (props) => {
+    const path = `/dialogs/${props.id}`;
+    return (
+        <div className={`${style.dialog} ${style.active}`}>
+            <NavLink to={path}>{props.userName}</NavLink>
+        </div>
+    );
+}
+
+const Message = (props) => {
+    return (
+        <div className={style.message}>{props.messageText}</div>
+    );
+}
+
 export const Dialogs = () => {
     return (
         <div className={style.wrapper}>
             <div className={style.dialogs}>
-                <div className={`${style.dialog} ${style.active}`}>
-                    <NavLink to="/dialogs/1">User 1</NavLink>
-                </div>
-                <div className={style.dialog}>
-                    <NavLink to="/dialogs/2">User 2</NavLink>
-                </div>
-                <div className={style.dialog}>
-                    <NavLink to="/dialogs/3">User 3</NavLink>
-                </div>
-                <div className={style.dialog}>
-                    <NavLink to="/dialogs/4">User 4</NavLink>
-                </div>
+                <Dialog id={1} userName="User 1" />
+                <Dialog id={2} userName="User 2" />
+                <Dialog id={3} userName="User 3" />
+                <Dialog id={4} userName="User 4" />
             </div>
             <div className={style.messages}>
-                <div className={style.message}>Hello User!</div>
-                <div className={style.message}>What's up</div>
-                <div className={style.message}>How are you doing?</div>
-                <div className={style.message}>I'm good.</div>
+                <Message messageText="Hello world!" />
+                <Message messageText="The world is great!" />
+                <Message messageText="What a beautiful day!!!" />
             </div>
         </div >
     );
