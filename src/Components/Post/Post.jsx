99e00ed736fdc85f87243dfaa23c1404de33ba57
vgeroim <vgeroim.qa@gmail.com>
2022-02-12 12:23:24 -0600

    add ref on post textarea

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,12 +1,20 @@
 import style from './Post.module.css';
+import React from 'react';
 
 export const Post = () => {
+    const areaRef = React.createRef();
+
+    const onClickHandler = () => {
+        alert(areaRef.current.value);
+        areaRef.current.value = "";
+    }
+
     return (
         <div className={style.posts}>
             <h2 className={style.title}>My posts</h2>
-            <div className={style.field}>
-            </div>
-            <a className={style.btn} href="#">Send</a>
+            <textarea ref={areaRef} className={style.field}>
+            </textarea>
+            <a className={style.btn} href="#" onClick={onClickHandler}>Send</a>
         </div>
     );
 }
